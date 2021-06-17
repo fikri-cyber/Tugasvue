00e29a0fe1873162074851f9f42a55f06fd4e876
fikri-cyber <fikrimuhammad206@gmail.com>
2021-06-17 16:53:33 +0700

    tugas

---
+++ src/pages/Home.vue
@@ -0,0 +1,29 @@
+<template>
+    <h1>Hello World</h1>
+        <h4>Priject Vue Pertama saya</h4>
+        <h4>Nama Saya {{ nama }}</h4>
+        <h4>Umur Saya {{ tahunlahir }} tahun </h4>
+        <input v-model="kotalahir"/>
+        <h5>Kota Lahir Saya : {{ kotalahir }} </h5>
+        <button v-on:click="next">Next</button>
+</template>
+
+<script>
+ export default {
+     data (){
+         return{
+             nama: "Muhammad Fikri",
+             tahunlahir : 1999,
+             kotalahir : "cimahi"
+         }
+     },
+     methods:{
+         next(){
+             this.tahunlahir += 1;
+         }
+     },
+     mounted(){
+         this.tahunlahir = 2021 - this.tahunlahir; 
+     }
+ }
+ </script>
---
+++ src/router/index.js
@@ -0,0 +1,36 @@
+import { createWebHistory, createRouter } from "vue-router";
+import Home from "@/pages/Home.vue";
+import About from "@/pages/About.vue";
+import Loops from "@/pages/Loops.vue";
+import Kalkulator from "@/pages/Kalkulator.vue";
+
+
+const routes = [
+    {
+        path: "/",
+        name: "Home",
+        component: Home,
+     },
+     {
+        path: "/about",
+        name: "About",
+        component: About,
+     },
+     {
+        path: "/loops",
+        name: "Loops",
+        component: Loops,
+     },
+     {
+         path: "/kalkulator",
+         name: "Kalkulator",
+         component: Kalkulator
+     }
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+export default router;
